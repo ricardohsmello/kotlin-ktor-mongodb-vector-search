a13ae14e267cc8f9d8ecf4c5db5edd6fd5758dda
Ricardo <ricardohsmello@gmail.com>
2024-03-11 23:37:57 -0300

    feat: adding routes and koin inject dependency in Application

--- src/main/kotlin/com/mongodb/domain/ports/ExerciseRepository.kt
+++ src/main/kotlin/com/mongodb/domain/ports/ExerciseRepository.kt
@@ -1,7 +1,6 @@
 package com.mongodb.domain.ports
 
 import com.mongodb.domain.entity.Exercises
-
 interface ExercisesRepository {
     suspend fun findSimilarExercises(embedding: List<Double>): List<Exercises>
 }
